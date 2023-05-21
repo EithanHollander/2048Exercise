f7fb19b8c89970296e1024b3924ecd1d2217fba7
Eithan Hollander <eithanhollander@gmail.com>
2023-05-21 11:49:19 +0300

    Display: added basic display of Matrix

--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -1,6 +1,27 @@
 import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import matrixStore from "../stores/MatrixStore";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 410px;
+  width: 410px;
+`;
+
+const Cell = styled.div`
+  border: solid black 1px;
+  height: 100px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 35px;
+`;
 
 const Matrix = observer(() => {
   useEffect(() => {
@@ -8,7 +29,21 @@ const Matrix = observer(() => {
     console.log(matrixStore.board);
   }, []);
 
-  return <div>This is Matrix!</div>;
+  return (
+    <Container>
+      {matrixStore.board.map((row, index) => {
+        return (
+          <div key={index}>
+            {row.map((cellValue, index) => {
+              return (
+                <Cell key={index}>{cellValue === 0 ? null : cellValue}</Cell>
+              );
+            })}
+          </div>
+        );
+      })}
+    </Container>
+  );
 });
 
 export default Matrix;
